938b2112b9d6e39013c75a948594c6878d1cd79e
Conn-Ho <2015954952@qq.com>
2024-11-21 17:56:42 +0800

    modified authRoutes.js(backend)

--- back_end/routes/authRoutes.js
+++ back_end/routes/authRoutes.js
@@ -1,12 +1,10 @@
-//路由 
+//定义路由 
 //API路径和请求方式
 
 import express from 'express';
 import authController from '../controllers/authController.js';
 
 const router = express.Router();
-
 router.post('/register', authController.register);
 router.post('/login', authController.login);
-
 export default router;
